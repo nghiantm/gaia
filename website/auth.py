b9e8f20e7946301ff37485efba9af6dcfc91f784
mn839 <mn839@drexel.edu>
2023-04-29 17:33:48 -0400

    update

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
-def login():
-    return "<p>Login<p>"
+@auth.route('/main')
+def main():
+    return render_template('main.html', bool=True)
 
 @auth.route('/logout')
 def logout():
--- website/views.py
+++ website/views.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import requests
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 def home():
+    if request.form.get('start') == 'clicked':
+        return render_template('main.html')
     return render_template('index.html')
