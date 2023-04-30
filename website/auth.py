6e25f7ad48a403a9772d7a3933576223455cf96b
mn839 <mn839@drexel.edu>
2023-04-30 00:12:24 -0400

    Update

--- website/auth.py
+++ website/auth.py
@@ -4,22 +4,36 @@ import csv
 
 auth = Blueprint('auth', __name__)
 
+def initDict():
+    global dailyDict
+    dailyDict = {
+        'Use reusable bags': False,
+        'Use reusable water bottles': False,
+        'Use natural for at least 8 hours': False,
+        'Walk or use public transports instead of drive': False,
+        'Turn off unused electronics': False,
+        'Limit your AC usage': False,
+        'Plan your meal for less food waste': False,
+        'Plant or take care of a tree': False,
+        'Take digital note rather than using paper': False,
+        'random': False
+    }
+
+initDict()
+
 @auth.route('/main', methods=['GET', 'POST'])
 def main():
-    dailyDict = {}
-    with open('website/static/dailyChallenge.csv') as daily:
-        dailyReader = csv.reader(daily, delimiter=',')
+    #running score for tree development
+    treeScore = 0
+    for key, value in dailyDict.items():
+        if dailyDict[key] == True:
+            treeScore += 1
+ 
+    if request.method == 'POST':
+        newKey = request.form.get('doneness')
+        #reverse boolean value when clicked
+        dailyDict[newKey] = not dailyDict[newKey]
+        #redirect to avoid duplicate POST request
+        return redirect('#')
 
-        #skip header row
-        next(dailyReader)
-    
-        for row in dailyReader:
-            key = row[0]
-            value = bool(row[1])
-            dailyDict[key] = value
-
-    return render_template('main.html', dailyDict=dailyDict)
-
-@auth.route('/sign-up')
-def sign_up():
-    return "<p>Sign up<p>"
+    return render_template('main.html', dailyDict=dailyDict, treeScore=treeScore)
