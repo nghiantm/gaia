f4301b063764834e7db171dbe6a2e3882f3e1fef
mn839 <mn839@drexel.edu>
2023-04-29 20:11:41 -0400

    Updated redirect

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 import requests
 
 views = Blueprint('views', __name__)
@@ -6,5 +6,5 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 def home():
     if request.form.get('start') == 'clicked':
-        return render_template('main.html')
+        return redirect(url_for('auth.main'))
     return render_template('index.html')
